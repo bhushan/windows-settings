0a145f3234d738aa27c87664695240ab28927fc5
Bhushan <bhushangaykawad@gmail.com>
2019-10-14 11:17:56 +0530

    wip

--- alias.cmd
+++ alias.cmd
@@ -1,15 +1,24 @@
 @echo off
 
 :: Commands
-DOSKEY alias=notepad d:\workspace\windows-settings\alias.cmd
+DOSKEY alias=code d:\workspace\windows-settings\alias.cmd
+
 DOSKEY work= d: && cd d:\workspace\
+
 DOSKEY ls=dir /B
+
 DOSKEY cc=cls
+
 DOSKEY ..=cd ..
 
 DOSKEY p=phpunit
+
 DOSKEY pf=phpunit --filter $* 
+
 DOSKEY art=php artisan $*
+
 DOSKEY migrate=php artisan migrate
+
 DOSKEY fresh=php artisan migrate:fresh
+
 DOSKEY seed=php artisan migrate:fresh --seed
